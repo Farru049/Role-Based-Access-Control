598bd3953cb17d1d2282621cb2637c9ae662b5b0
Farhaan Ali <alifarhaan655@gmail.com>
2024-11-22 11:39:00 +0530

    newchanges23

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+// Get the base URL from the homepage property in package.json
+const basename = process.env.PUBLIC_URL || '/';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
