ca24375e7ee893e93bbca9bf2edbd3729db87c9f
Farhaan Ali <alifarhaan655@gmail.com>
2024-11-22 11:31:43 +0530

    newchanges2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Path to all your jsx/ts files
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
   theme: {
     extend: {},
